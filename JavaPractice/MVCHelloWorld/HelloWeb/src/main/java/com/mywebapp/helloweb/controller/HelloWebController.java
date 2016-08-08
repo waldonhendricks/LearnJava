480e25092b9b039721851142f4e8867d5086ae16
SyncMr <talk2pragadheesh@gmail.com>
2016-08-08 02:11:24 -0700

    Adding Sample Spring MVC Project

---
+++ JavaPractice/MVCHelloWorld/HelloWeb/src/main/java/com/mywebapp/helloweb/controller/HelloWebController.java
@@ -0,0 +1,23 @@
+package com.mywebapp.helloweb.controller;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+ 
+@Controller
+public class HelloWebController {
+	String message = "Welcome to Spring MVC!";
+ 
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+ 
+		ModelAndView mv = new ModelAndView("helloweb");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+}
