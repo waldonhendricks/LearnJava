91f7e8737bb06545a7eed95add728260cf1b474f
SyncMr <talk2pragadheesh@gmail.com>
2016-10-07 11:36:17 -0700

    Java mongo db

---
+++ JavaPractice/MongoDB/MongoDB/src/main/java/org/pragad/mongodb/MongoDB/App.java
@@ -0,0 +1,30 @@
+package org.pragad.mongodb.MongoDB;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.ServerAddress;
+
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+
+import org.bson.Document;
+
+import com.mongodb.client.MongoCursor;
+import static com.mongodb.client.model.Filters.*;
+import com.mongodb.client.result.DeleteResult;
+import static com.mongodb.client.model.Updates.*;
+import com.mongodb.client.result.UpdateResult;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! 123" );
+    }
+}
